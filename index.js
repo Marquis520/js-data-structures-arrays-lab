f00669a56896c5016e8d6501ed0d43a9cce90e55
Marquis520 <Marquis520@icloud.com>
2021-04-03 19:17:13 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield']
+beforeEach(function () {
+    drivers.length = 0;
+
+    drivers.push('Milo', 'Otis', 'Garfield');
+  });
+function destructivelyAppendDriver(){
+    drivers.push('Ralph');
+}
+function destructivelyPrependDriver(){
+    drivers.unshift('Bob');
+}
+function destructivelyRemoveLastDriver(){
+    drivers.pop();
+}
+function destructivelyRemoveFirstDriver(){
+    drivers.shift();
+}
+function appendDriver(name){
+    const newDrivers = [...drivers, name];
+        return newDrivers;
+}
+function prependDriver(name){
+    const newDrivers = [name, ...drivers]
+        return newDrivers;
+}
+function removeLastDriver(){
+    return drivers.slice(0, drivers.length - 1);
+}
+function removeFirstDriver(){
+    return drivers.slice(1);
+}
